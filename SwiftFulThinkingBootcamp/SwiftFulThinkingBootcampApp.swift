d88863a0ff97d1e85bdf699d212dae9dd18e699e
Louis Rey <louisreypro@yahoo.com>
2022-10-04 12:12:59 +0200

    Initial Commit

---
+++ SwiftFulThinkingBootcamp/SwiftFulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFulThinkingBootcampApp.swift
+//  SwiftFulThinkingBootcamp
+//
+//  Created by Louis REY on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
