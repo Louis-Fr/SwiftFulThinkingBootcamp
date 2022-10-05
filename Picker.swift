02e3aeabe45b084345967488e728914fb8995c87
Louis Rey <louisreypro@yahoo.com>
2022-10-05 05:33:33 +0200

    Add Picker and ForEach files

---
+++ Picker.swift
@@ -0,0 +1,20 @@
+//
+//  Picker.swift
+//  SwiftFulThinkingBootcamp
+//
+//  Created by Louis REY on 05/10/2022.
+//
+
+import SwiftUI
+
+struct Picker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Picker_Previews: PreviewProvider {
+    static var previews: some View {
+        Picker()
+    }
+}
